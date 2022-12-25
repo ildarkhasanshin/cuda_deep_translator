34b037fc6293a160400bca7c8e7a29ad5bb90763
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2022-12-25 20:38:04 +0500

    [*] fix issue#2

--- __init__.py
+++ __init__.py
@@ -3,21 +3,35 @@ from cudatext import *
 from cudax_lib import get_translation
 _ = get_translation(__file__)
 
+try:
+    from deep_translator import GoogleTranslator
+    deep_translator_imported = True
+except:
+    deep_translator_imported = False
+    msg_box(_('Install first') + ': pip install -U deep-translator', MB_OK+MB_ICONERROR)
+
+
 class Command:
-    def translate(self):
-        try:
-            from deep_translator import GoogleTranslator
+    def translate(self, txt):
+        langs_list = GoogleTranslator().get_supported_languages()
+        lst = ''
+        for i in langs_list:
+            lst = lst + i + "\n"
+        res = dlg_menu(DMENU_LIST, lst, 0, _('Supported languages'))
+        if res is None: return
+        return GoogleTranslator(source='auto', target=langs_list[res]).translate(txt)
+
+    def res2tab(self):
+        if deep_translator_imported:
             txt = ed.get_text_sel()
-            if len(txt) >0:
-                langs_list = GoogleTranslator().get_supported_languages()
-                lst = ''
-                for i in langs_list:
-                    lst = lst + i + "\n"
-                res = dlg_menu(DMENU_LIST, lst, 0, _('Supported languages'))
-                if res is None: return
-                lang = langs_list[res]
-                translated = GoogleTranslator(source='auto', target=lang).translate(txt)
+            if len(txt) > 0:
+                translated = self.translate(txt)
                 file_open('')
                 ed.set_text_all(translated)
-        except ImportError:
-            msg_box(_('Install first') + ': pip install -U deep-translator', MB_OK+MB_ICONERROR)
+
+    def res2msgbox(self):
+        if deep_translator_imported:
+            txt = ed.get_text_sel()
+            if len(txt) > 0:
+                translated = self.translate(txt)
+                msg_box(translated, MB_OK+MB_ICONINFO)
